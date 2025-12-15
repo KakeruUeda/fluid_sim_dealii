4e2fde30159321d19a898124a4e9dd60d603ff7f
Kakeru <kakecoco2580@icloud.com>
2025-12-16 00:14:02 +0900

    organize script file

--- script/build.sh
+++ script/build.sh
@@ -2,9 +2,13 @@ cd ../
 mkdir build
 cd build
 
-cmake -DCMAKE_INSTALL_PREFIX=/work/lab/science_tokyo/fluid_sim_dealii/bin \
+cmake -DCMAKE_INSTALL_PREFIX=$PWD/example \
+      -DCMAKE_C_COMPILER=mpicc \
+      -DCMAKE_CXX_COMPILER=mpicxx \
       -DCMAKE_BUILD_TYPE=Release \
       -DDIM_2D=ON \
       ..
 
-make -j && make install
+make -j 
+make install
+
